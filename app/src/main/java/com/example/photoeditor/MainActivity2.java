ab56a5b1bebb465ce1dc4313410cf0f62bb30c57
GowthamGoush <westcity.21.gowtham@gmail.com>
2020-08-29 16:38:31 +0530

    implements_blur

--- app/src/main/java/com/example/photoeditor/MainActivity2.java
+++ app/src/main/java/com/example/photoeditor/MainActivity2.java
@@ -6,14 +6,25 @@ import androidx.core.content.ContextCompat;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
 import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.Region;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
@@ -27,17 +38,19 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 public class MainActivity2 extends AppCompatActivity {
 
     private ImageView imageView;
-    private Button button,buttonSave;
+    private ImageButton button,buttonSave,buttonRotate,buttonBlur,buttonInsert;
     private Bitmap bitmap;
     private int width = 0;
     private int height = 0;
     private static final int MAX_PIXEL_COUNT = 2048;
     private int[] pixels;
     private int pixelCount = 0;
+    private int rotateIt = 90;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,13 +65,40 @@ public class MainActivity2 extends AppCompatActivity {
         imageView = findViewById(R.id.imageCrop);
         button = findViewById(R.id.colorBtn);
         buttonSave = findViewById(R.id.SaveBtn);
-/*
-        Glide.with(MainActivity2.this)
-                .load(path)
-                .placeholder(R.drawable.ic_launcher_background).centerCrop()
-                .into(imageView);
+        buttonRotate = findViewById(R.id.RotateBtn);
+        buttonBlur = findViewById(R.id.BlurBtn);
+        buttonInsert = findViewById(R.id.insertBtn);
 
- */
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity2.this,MainActivity.class);
+                startActivity(intent1);
+            }
+        });
+
+        buttonBlur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bitmap = imageBlur(bitmap,0.8f,1);
+                width = bitmap.getWidth();
+                height = bitmap.getHeight();
+                imageView.setImageBitmap(bitmap);
+            }
+        });
+
+        buttonRotate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Matrix matrix = new Matrix();
+                matrix.postRotate(rotateIt);
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, width, height, matrix, true);
+                width = bitmap.getWidth();
+                height = bitmap.getHeight();
+                imageView.setImageBitmap(bitmap);
+            }
+        });
+
         buttonSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -82,7 +122,7 @@ public class MainActivity2 extends AppCompatActivity {
                 new Thread(){
                     public void run(){
                         for (int i =0;i<pixelCount;i++){
-                           pixels[i] /=2;
+                           pixels[i] /=1.1;
                         }
                         bitmap.setPixels(pixels,0,width,0,0,width,height);
 
@@ -97,69 +137,70 @@ public class MainActivity2 extends AppCompatActivity {
             }
         });
 
-        new Thread(){
-            public void run(){
-                bitmap = null;
-                final BitmapFactory.Options bmpOptions = new BitmapFactory.Options();
-                bmpOptions.inBitmap = bitmap;
-                bmpOptions.inJustDecodeBounds = true;
+        if(path != null) {
 
-                try(InputStream input = getContentResolver().openInputStream(uri)) {
-                    bitmap = BitmapFactory.decodeStream(input,null,bmpOptions);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            new Thread() {
+                public void run() {
+                    bitmap = null;
+                    final BitmapFactory.Options bmpOptions = new BitmapFactory.Options();
+                    bmpOptions.inBitmap = bitmap;
+                    bmpOptions.inJustDecodeBounds = true;
 
-                bmpOptions.inJustDecodeBounds = false;
-                width = bmpOptions.outWidth;
-                height = bmpOptions.outHeight;
-
-                int resizeScale = 1;
-
-                if(width > MAX_PIXEL_COUNT){
-                    resizeScale = width/MAX_PIXEL_COUNT;
-                }
-                else if(height > MAX_PIXEL_COUNT){
-                    resizeScale = width/MAX_PIXEL_COUNT;
-                }
-
-                if(width/resizeScale > MAX_PIXEL_COUNT || height/resizeScale > MAX_PIXEL_COUNT){
-                    resizeScale++;
-                }
-
-                bmpOptions.inSampleSize = resizeScale;
-                InputStream input = null;
-
-                try {
-                    input = getContentResolver().openInputStream(uri);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                    recreate();
-                    return;
-                }
-
-                bitmap = BitmapFactory.decodeStream(input,null,bmpOptions);
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        imageView.setImageBitmap(bitmap);
+                    try (InputStream input = getContentResolver().openInputStream(uri)) {
+                        bitmap = BitmapFactory.decodeStream(input, null, bmpOptions);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-                });
 
-                width = bitmap.getWidth();
-                height = bitmap.getHeight();
+                    bmpOptions.inJustDecodeBounds = false;
+                    width = bmpOptions.outWidth;
+                    height = bmpOptions.outHeight;
 
-                bitmap = bitmap.copy(Bitmap.Config.ARGB_8888,true);
+                    int resizeScale = 1;
 
-                pixelCount = width*height;
-                pixels = new int[pixelCount];
+                    if (width > MAX_PIXEL_COUNT) {
+                        resizeScale = width / MAX_PIXEL_COUNT;
+                    } else if (height > MAX_PIXEL_COUNT) {
+                        resizeScale = width / MAX_PIXEL_COUNT;
+                    }
 
-                bitmap.getPixels(pixels,0,width,0,0,width,height);
-            }
-        }.start();
+                    if (width / resizeScale > MAX_PIXEL_COUNT || height / resizeScale > MAX_PIXEL_COUNT) {
+                        resizeScale++;
+                    }
 
+                    bmpOptions.inSampleSize = resizeScale;
+                    InputStream input = null;
+
+                    try {
+                        input = getContentResolver().openInputStream(uri);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                        recreate();
+                        return;
+                    }
+
+                    bitmap = BitmapFactory.decodeStream(input, null, bmpOptions);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            imageView.setImageBitmap(bitmap);
+                        }
+                    });
+
+                    width = bitmap.getWidth();
+                    height = bitmap.getHeight();
+
+                    bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+
+                    pixelCount = width * height;
+                    pixels = new int[pixelCount];
+
+                    bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
+                }
+            }.start();
+        }
     }
 
     private File createImageFile(){
@@ -168,6 +209,213 @@ public class MainActivity2 extends AppCompatActivity {
         final File storage = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
 
         return  new File(storage + name);
+    }
+
+
+    public Bitmap imageBlur(Bitmap sentBitmap, float scale, int radius) {
+
+        int width = Math.round(sentBitmap.getWidth() * scale);
+        int height = Math.round(sentBitmap.getHeight() * scale);
+        sentBitmap = Bitmap.createScaledBitmap(sentBitmap, width, height, false);
+
+        Bitmap bitmap = sentBitmap.copy(sentBitmap.getConfig(), true);
+
+        if (radius < 1) {
+            return (null);
+        }
+
+        int w = bitmap.getWidth();
+        int h = bitmap.getHeight();
+
+        int[] pix = new int[w * h];
+        bitmap.getPixels(pix, 0, w, 0, 0, w, h);
+
+        int wm = w - 1;
+        int hm = h - 1;
+        int wh = w * h;
+        int div = radius + radius + 1;
+
+        int r[] = new int[wh];
+        int g[] = new int[wh];
+        int b[] = new int[wh];
+        int rsum, gsum, bsum, x, y, i, p, yp, yi, yw;
+        int vmin[] = new int[Math.max(w, h)];
+
+        int divsum = (div + 1) >> 1;
+        divsum *= divsum;
+        int dv[] = new int[256 * divsum];
+        for (i = 0; i < 256 * divsum; i++) {
+            dv[i] = (i / divsum);
+        }
+
+        yw = yi = 0;
+
+        int[][] stack = new int[div][3];
+        int stackpointer;
+        int stackstart;
+        int[] sir;
+        int rbs;
+        int r1 = radius + 1;
+        int routsum, goutsum, boutsum;
+        int rinsum, ginsum, binsum;
+
+        for (y = 0; y < h; y++) {
+            rinsum = ginsum = binsum = routsum = goutsum = boutsum = rsum = gsum = bsum = 0;
+            for (i = -radius; i <= radius; i++) {
+                p = pix[yi + Math.min(wm, Math.max(i, 0))];
+                sir = stack[i + radius];
+                sir[0] = (p & 0xff0000) >> 16;
+                sir[1] = (p & 0x00ff00) >> 8;
+                sir[2] = (p & 0x0000ff);
+                rbs = r1 - StrictMath.abs(i);
+                rsum += sir[0] * rbs;
+                gsum += sir[1] * rbs;
+                bsum += sir[2] * rbs;
+                if (i > 0) {
+                    rinsum += sir[0];
+                    ginsum += sir[1];
+                    binsum += sir[2];
+                } else {
+                    routsum += sir[0];
+                    goutsum += sir[1];
+                    boutsum += sir[2];
+                }
+            }
+            stackpointer = radius;
+
+            for (x = 0; x < w; x++) {
+
+                r[yi] = dv[rsum];
+                g[yi] = dv[gsum];
+                b[yi] = dv[bsum];
+
+                rsum -= routsum;
+                gsum -= goutsum;
+                bsum -= boutsum;
+
+                stackstart = stackpointer - radius + div;
+                sir = stack[stackstart % div];
+
+                routsum -= sir[0];
+                goutsum -= sir[1];
+                boutsum -= sir[2];
+
+                if (y == 0) {
+                    vmin[x] = Math.min(x + radius + 1, wm);
+                }
+                p = pix[yw + vmin[x]];
+
+                sir[0] = (p & 0xff0000) >> 16;
+                sir[1] = (p & 0x00ff00) >> 8;
+                sir[2] = (p & 0x0000ff);
+
+                rinsum += sir[0];
+                ginsum += sir[1];
+                binsum += sir[2];
+
+                rsum += rinsum;
+                gsum += ginsum;
+                bsum += binsum;
+
+                stackpointer = (stackpointer + 1) % div;
+                sir = stack[(stackpointer) % div];
+
+                routsum += sir[0];
+                goutsum += sir[1];
+                boutsum += sir[2];
+
+                rinsum -= sir[0];
+                ginsum -= sir[1];
+                binsum -= sir[2];
+
+                yi++;
+            }
+            yw += w;
+        }
+        for (x = 0; x < w; x++) {
+            rinsum = ginsum = binsum = routsum = goutsum = boutsum = rsum = gsum = bsum = 0;
+            yp = -radius * w;
+            for (i = -radius; i <= radius; i++) {
+                yi = Math.max(0, yp) + x;
+
+                sir = stack[i + radius];
+
+                sir[0] = r[yi];
+                sir[1] = g[yi];
+                sir[2] = b[yi];
+
+                rbs = r1 - StrictMath.abs(i);
+
+                rsum += r[yi] * rbs;
+                gsum += g[yi] * rbs;
+                bsum += b[yi] * rbs;
+
+                if (i > 0) {
+                    rinsum += sir[0];
+                    ginsum += sir[1];
+                    binsum += sir[2];
+                } else {
+                    routsum += sir[0];
+                    goutsum += sir[1];
+                    boutsum += sir[2];
+                }
+
+                if (i < hm) {
+                    yp += w;
+                }
+            }
+            yi = x;
+            stackpointer = radius;
+            for (y = 0; y < h; y++) {
+                // Preserve alpha channel: ( 0xff000000 & pix[yi] )
+                pix[yi] = ( 0xff000000 & pix[yi] ) | ( dv[rsum] << 16 ) | ( dv[gsum] << 8 ) | dv[bsum];
+
+                rsum -= routsum;
+                gsum -= goutsum;
+                bsum -= boutsum;
+
+                stackstart = stackpointer - radius + div;
+                sir = stack[stackstart % div];
+
+                routsum -= sir[0];
+                goutsum -= sir[1];
+                boutsum -= sir[2];
+
+                if (x == 0) {
+                    vmin[y] = Math.min(y + r1, hm) * w;
+                }
+                p = x + vmin[y];
+
+                sir[0] = r[p];
+                sir[1] = g[p];
+                sir[2] = b[p];
+
+                rinsum += sir[0];
+                ginsum += sir[1];
+                binsum += sir[2];
+
+                rsum += rinsum;
+                gsum += ginsum;
+                bsum += binsum;
+
+                stackpointer = (stackpointer + 1) % div;
+                sir = stack[stackpointer];
+
+                routsum += sir[0];
+                goutsum += sir[1];
+                boutsum += sir[2];
+
+                rinsum -= sir[0];
+                ginsum -= sir[1];
+                binsum -= sir[2];
+
+                yi += w;
+            }
+        }
+
+        bitmap.setPixels(pix, 0, w, 0, 0, w, h);
+
+        return (bitmap);
     }
 
 }
